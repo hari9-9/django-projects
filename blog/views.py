95042818a5f4061f9c9225a390ed99c29ba704b5
hari9_9 <reachk.hariharan@gmail.com>
2020-04-21 17:58:17 +0530

    added more html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts=[
+    {
+        'author': 'Hari',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'aug 27,2018'
+
+    },
+    {
+        'author': 'random dude',
+        'title':'Blog post 2',
+        'content':'secondpost content',
+        'date_posted':'aug 27,2018'
+
+    }
+]
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 
 def about(request):
